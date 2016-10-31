eb4c349c0c9b1ef9824e5a3e95c697578f1fee2a
ruLegen <forgame98@yandex.ru>
2016-10-31 20:19:29 +0400

    Revert "Revert "FirstCommit""
    This reverts commit 10c493ce8c7ffa685b0afda3c8516b21feef383d.

---
+++ Windows Server/ControlServer/ControlServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ControlServer
+{
+    class Server
+    {
+        TcpListener Listener;
+
+public  Server(int port)
+        {
+            Listener = new TcpListener(IPAddress.Any,port);
+            Listener.Start();
+           
+        }
+        ~Server()
+        {
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
